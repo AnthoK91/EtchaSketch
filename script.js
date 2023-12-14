21399a42696fb27b71ecf81a7a84227e504d9411
Anthony Kirkham <antho.kirkham@gmail.com>
2023-12-14 22:20:50 +1000

    finished product

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 //Select the container
 const container = document.querySelector('#container')
+const buttonSelect = document.querySelector('#restartButton')
+const outerBox = document.querySelector('#outerBox')
 
+createGrid(10);
+gridSize(10);
 
-//set Div colour
+//button selection
+buttonSelect.addEventListener("click", () => {
+    let clickEvent = prompt("How many squares would you like per row?")
+    //remove the mini boxes
+    const miniBoxes = document.querySelectorAll('.miniBox');
+    miniBoxes.forEach((miniBox) => {
+    miniBox.remove();
+    })
+    createGrid(clickEvent);
+    gridSize(clickEvent);
 
-  
+})
 
+//Change the Grid Size
+function gridSize(clickEvent) {
+    const miniBox = document.querySelectorAll('.miniBox');
+    miniBox.forEach((boxes) => {
+        boxes.style.minWidth = 800 / clickEvent + 'px';
+        boxes.style.maxWidth = 800 / clickEvent + 'px';
+        boxes.style.minHeight = 800 / clickEvent + 'px';
+        boxes.style.minHeight = 800 / clickEvent + 'px';
+    })
+}
 
 //Create the divs and assign the css
 function createDiv() {
 const div = document.createElement('div');
 div.classList.add('miniBox')
-div.textContent = 'Hello World 2'
 container.appendChild(div);
-
 }
 
+function createGrid(clickEvent) {
 //for loop creating 16 Divs
-for (let i = 1; i < 17; i++) {
+for (let i = 0; i < clickEvent * clickEvent; i++) {
     createDiv();
-    console.log('16 times' + i);
+}
+    addMouseoverEffect();
 }
 
-
-
-// the JavaScript file
+function addMouseoverEffect() {
+// mouseover effect for all divs - Query Select All > Foreach on all divs
 const divColour = document.querySelectorAll('.miniBox');
 divColour.forEach((divBox) => {
     divBox.addEventListener("mouseover", () => {
         const randomColor = Math.floor(Math.random()*16777215).toString(16);
         divBox.style.backgroundColor = "#" + randomColor;
     })})
+}
